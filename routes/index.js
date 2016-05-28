d2f9c2b455d760237e2f5e70a1ef43220000fcd3
Alex Bertrand <alex.bertrand13@gmail.com>
2016-05-27 21:30:23 -0400

    More error fixing.

--- routes/index.js
+++ routes/index.js
@@ -15,14 +15,12 @@ router.get('/test', function(req, res, next) {
 	res.render('index', {title: 'HELLO WORLD!'});
 });
 
-router.get('/memories', function(req, res, next) {	
-	console.log('[alex] database connection string:' + connectionString);	
-	
+router.get('/memories', function(req, res, next) {		
 	// connect a client	
 	var client = new pg.Client(connectionString);
 	client.connect(function(err) {
 		if(err) {
-			console.log('[alex] error in connecting to database:' + err);
+			console.log('[alex] error in connecting to database, trying to fetch:' + err);
 
 			// Major bodge here.
 			res.send([
@@ -39,14 +37,13 @@ router.get('/memories', function(req, res, next) {
 	var results = [];
 
 	// connect to db and pull memories
-	var query = client.query("SELECT * FROM memories"); // dead simple, mate
+	var query = client.query("SELECT * FROM memories ORDER BY id DESC LIMIT 20"); // dead simple, mate
 	query.on('row', function(data) {
 		results.push(data);	
 	});
 
 	query.on('end', function() {
 		client.end();
-		console.log(results);
 		return res.json(results);
 	});
 });
@@ -61,22 +58,34 @@ router.post('/submit', function(req, res) {
 
 	// INSERT VALIDATION HERE
 
-	console.log(place, memory);
+	console.log('[alex] place and memory to be inserted: ' + place, memory);
 
 	var client = new pg.Client(connectionString);
-	client.connect();
+	client.connect(function(err) {			
+		if(err) {
+			console.log('[alex] error in connecting to database, trying to insert: ' + err);
+
+			// In absence of better things, we're just going to ignore the fact that this didn't work
+			client.end();
+			res.redirect('error');
+			res.end();
+		}
+	});
 
 	var sql = "INSERT INTO memories(place, memory) values($1, $2)";
 	var params = [place, memory];
 
 	var query = client.query(sql, params, function(err, result) {
-		console.log(err);	
-		console.log(result);
+		if(err) {
+			console.log('[alex] error querying to insert memory: ' + err);
+			// we should probably return an error page or something
+		}
 	});
 
 	query.on('end', function() {
 		client.end();
-		res.render('home');
+		res.redirect('/');
+		res.end();
 	});
 });
 
